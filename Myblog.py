5f33702d5df8e82f7aa3a09876dac15cd2307103
HymanLiu1987 <879651072@qq.com>
2016-09-22 23:44:10 +0800

    version 1.0

---
+++ Myblog.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from flask import Flask,render_template
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
+app=Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment=Moment(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+
+if __name__=='__main__':
+    manager.run()
