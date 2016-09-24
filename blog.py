d2f604dbf003a127f4af77b70623c9c2e3dcd9ae
HymanLiu1987 <879651072@qq.com>
2016-09-24 16:09:07 +0800

    version 2.3

--- blog.py
+++ blog.py
@@ -18,6 +18,7 @@ class NameForm(Form):
     name=StringField('what is your name?',validators=[Required()])
     submit=SubmitField('Submit')
 
+
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
@@ -29,6 +30,23 @@ db=SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment=Moment(app)
+
+
+class Role(db.Model):
+    __tablename__='role'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role')
+    def __repr__(self):
+        return '<Role %r>'% self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+    def __repr__(self):
+        return '<User %r>'% self.username
 
 
 @app.route('/',methods=['GET','POST'])
