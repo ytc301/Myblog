912cd027c1bfae260a7428fbe6db23aad7708b9f
HymanLiu1987 <879651072@qq.com>
2016-09-25 11:47:38 +0800

    version 2.3

--- blog.py
+++ blog.py
@@ -12,6 +12,8 @@ from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Shell
+from flask.ext.migrate import Migrate,MigrateCommand
 import os
 
 class NameForm(Form):
@@ -30,13 +32,14 @@ db=SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment=Moment(app)
-
+migrate=Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 class Role(db.Model):
     __tablename__='role'
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
-    users=db.relationship('User',backref='role')
+    users=db.relationship('User',backref='role',lazy='dynamic')
     def __repr__(self):
         return '<Role %r>'% self.name
 
@@ -54,12 +57,23 @@ def index():
     name=None
     form=NameForm()
     if form.validate_on_submit():
-        old_name=session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('name incorrect')
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user =User(username=form.name.data)
+            db.session.add(user)
+            session['konwn'] = False
+        else:
+            session['known'] = True
         session['name']= form.name.data
+        form.name.data=''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'))
+    return render_template('index.html',form=form,name=session.get('name'),
+            known=session.get('known',False))
+
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+
+
 
 @app.route('/user/<name>')
 def user(name):
@@ -74,4 +88,5 @@ def internal_server_error(e):
 
 
 if __name__=='__main__':
+    manager.add_command("shell",Shell(make_context=make_shell_context))
     manager.run()
