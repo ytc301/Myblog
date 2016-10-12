98489ab67848552e93570539bad9681f241b5eb2
HymanLiu1987 <879651072@qq.com>
2016-10-13 00:19:13 +0800

    adjust dir construct

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLALchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap=Bootstrap()
@@ -12,7 +12,7 @@ db=SQLAlchemy()
 
 def create_app(config_name):
     app=Flask(__name__)
-    app.config.form_object(config[config_name])
+    app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
