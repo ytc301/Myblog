019fd7b0343410da197658d939f15aacb9a04778
HymanLiu1987 <879651072@qq.com>
2016-10-23 23:28:56 +0800

    add user login

---
+++ app/auth/views.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from . import auth
+from .forms import LoginForm
+
+@auth.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('auth/login.html',form=form);
--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
+#! -*-coding:utf-8-*-
+
 from app import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 class Role(db.Model):
     __tablename__='role'
     id=db.Column(db.Integer,primary_key=True)
@@ -7,10 +13,32 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>'% self.name
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__='users'
     id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(64),unique=True,index=True)
     username=db.Column(db.String(64),unique=True,index=True)
     role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+    password_hash=db.String(128)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
     def __repr__(self):
         return '<User %r>'% self.username
+    #修饰器，把方法做成属性
+    @property
+    def password(self):
+        raise AttributeError('passwoed is not a readable attribute')
+    
+    #设置属性的写
+    @password.setter
+    def password(self,password): 
+	self.password_hash=generate_password_hash(password)
+
+    #比较用户输入的密码和保存的密码
+    def verify_password(self,password):
+	return check_password_hash(self.password_hash,password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
