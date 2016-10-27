6050c9c9d09f9d439080b250b513297e58f93847
HymanLiu1987 <879651072@qq.com>
2016-10-27 23:51:12 +0800

    完成登出登入工具

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,22 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from .forms import LoginForm
+from ..models import User
 
 @auth.route('/login')
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remeber_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invaild user name or password')
     return render_template('auth/login.html',form=form);
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
