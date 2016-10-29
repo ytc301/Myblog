1ce3cac208878328fb99edc9d336231c6e111dde
HymanLiu1987 <879651072@qq.com>
2016-10-29 23:57:46 +0800

    增加注册用户功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template,request,redirect,url_for,flash
 from flask_login import login_user,logout_user,login_required
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
 from ..models import User
+from app import db
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -20,3 +21,13 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
