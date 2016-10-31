a47712dc653b68d84f509e669f98a1056952a37d
HymanLiu1987 <879651072@qq.com>
2016-11-01 00:03:45 +0800

    初步完成邮件确认功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
 #!_*_ coding:utf-8 _*_
 from flask import render_template,request,redirect,url_for,flash
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
 from . import auth
 from .forms import LoginForm,RegistrationForm
 from ..models import User
 from app import db
 from ..email import send_email
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -36,3 +37,36 @@ def register():
         flash('A confirmation email has been sent to you by email')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html',form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account.Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated  \
+            and not current_user.confirmed \
+            and request.endpoint[:5]!='auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token=current_user.generate_confirmation_token()
+    send_email(current_user.email,'Confirm your Account','auth/email/confirm',user=current_user,token=token)
+    flash('A new confirmation email has been sent to you by email')
+    return redirect(url_for('main.index'))
--- app/email.py
+++ app/email.py
@@ -1,4 +1,6 @@
 from flask_mail import Message
+from app import mail
+from flask import render_template
 class MyMessage(Message):
     def __init__(self,subject,sender,recipients):
         super(MyMessage,self).__init__(subject,sender=sender,recipients=recipients)
@@ -8,5 +10,11 @@ msg=MyMessage('mysubject','879651072@qq.com',['879651072@qq.com'])
 msg.body='text body'
 msg.html='<b>HTML</b> body'
 
+
+
+
 def send_email(to,subject,template,**kwargs):
-    pass
+    msg=Message("*TecnologyDreamer*"+subject,sender='879651072@qq.com',recipients=[to])
+    msg.body=render_template(template+'.txt',**kwargs)
+    msg.html=render_template(template+'.html',**kwargs)
+    mail.send
