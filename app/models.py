47ed93bec52f9cf0ff8d8b3078533102d9ab2f82
HymanLiu1987 <879651072@qq.com>
2016-11-05 14:08:55 +0800

    模型中增加角色

--- app/models.py
+++ app/models.py
@@ -7,13 +7,48 @@ from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 
+class Permission:
+    FOLLOW=0x01
+    COMMIT=0x02
+    WRITE_ARTICLES=0x04
+    MODERATE_COMMENTS=0x08
+    ADMINISTER=0x80
+
 class Role(db.Model):
     __tablename__='roles'
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
+    defult = db.Column(db.Boolean,default=False,index=True)
+    permissions=db.Column(db.Integer)
     users=db.relationship('User',backref='role',lazy='dynamic')
+
     def __repr__(self):
         return '<Role %r>'% self.name
+    
+    @staticmethod
+    def insert_roles():
+        roles={
+            'User':(Permission.FOLLOW|
+                    Permission.COMMIT|
+                    Permission.WRITE_ARTICLES,True),
+
+            'Moderator':(
+                    Permission.FOLLOW|
+                    Permission.COMMIT|
+                    Permission.WRITE_ARTICLES|
+                    Permission.MODERATE_COMMENTS,False),
+
+            'Administrator':(0xff,False),
+            }
+
+        for r in roles:
+            role =Role.query.filter_by(name=r).first()
+            if role is None:
+                role=Role(name=r)
+            role.permissions=roles[r][0]
+            role.default=roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 class User(UserMixin,db.Model):
     __tablename__='users'
@@ -23,9 +58,18 @@ class User(UserMixin,db.Model):
     password_hash=db.Column(db.String(128))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
     confirmed=db.Column(db.Boolean,default=False)
+    
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.email==current_app.config['FLASKY_ADMIN']:
+                self.role=Role.query.filter_by(permissions=0xff).first()
+            else:
+                self.role=Role.query.filter_by(default=True).first()
 
     def __repr__(self):
         return '<User %r>'% self.username
+
     #修饰器，把方法做成属性
     @property
     def password(self):
