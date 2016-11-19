9b6bf442e2d1358d24d110edf70f71669b947392
HymanLiu1987 <879651072@qq.com>
2016-11-19 19:06:57 +0800

    数据库模型中增加关注者

--- app/models.py
+++ app/models.py
@@ -18,6 +18,12 @@ class Permission:
     MODERATE_COMMENTS=0x08
     ADMINISTER=0x80
 
+class Follow(db.Model):
+    __tablename__='follows'
+    follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    timestamp = db.Column(db.DateTime,default=datetime.utcnow)
+
 class Role(db.Model):
     __tablename__='roles'
     id=db.Column(db.Integer,primary_key=True)
@@ -69,6 +75,16 @@ class User(UserMixin,db.Model):
     last_seen=db.Column(db.DateTime(),default=datetime.utcnow)
     avatar_hash=db.Column(db.String(32))
     posts=db.relationship('Post',backref='author',lazy='dynamic')
+    followed=db.relationship('Follow',
+                             foreign_keys=[Follow.follower_id],
+                              backref=db.backref('follower',lazy='joined'),
+                              lazy='dynamic',
+                              cascade='all,delete-orphan')
+    followers=db.relationship('Follow',
+                             foreign_keys=[Follow.followed_id],
+                              backref=db.backref('followed',lazy='joined'),
+                              lazy='dynamic',
+                              cascade='all,delete-orphan')
 
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
@@ -165,6 +181,22 @@ class User(UserMixin,db.Model):
             except IntegrityError:
                 db.session.rollback()
 
+    def follow(self,user):
+        if not self.is_following(user):
+            f=Follow(follower=self,followed=user)
+            db.session.add(f)
+
+    def unfollow(self,user):
+        f = self.followes.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self,user):
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+    
+    def is_followed_by(self,user):
+        return self.followers.filter_by(follower_id=user.id).first() is not None
+
 class AnonymousUser(AnonymousUserMixin):
     def can(self,permissions):
         return False
@@ -205,8 +237,6 @@ class Post(db.Model):
         target.body_html=bleach.linkify(bleach.clean(markdown(value,output_format='html'),tags=allowed_tags,strip=True))
 
 db.event.listen(Post.body,'set',Post.on_changed_body)
-
-
 
 
 
