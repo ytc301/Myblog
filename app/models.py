2f45712093a5bbdba6c2146ad1937a538e8e1300
HymanLiu1987 <879651072@qq.com>
2016-11-24 23:44:28 +0800

    增加用户评论的数据库模型

--- app/models.py
+++ app/models.py
@@ -85,6 +85,7 @@ class User(UserMixin,db.Model):
                               backref=db.backref('followed',lazy='joined'),
                               lazy='dynamic',
                               cascade='all,delete-orphan')
+    comments=db.relationship('Comment',backref='author',lazy='dynamic')
 
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
@@ -230,6 +231,7 @@ class Post(db.Model):
     body_html=db.Column(db.Text)
     timestamp=db.Column(db.DateTime,index=True,default=datetime.utcnow)
     author_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    comments=db.relationship('Comment',backref='post',lazy='dynamic')
 
     @staticmethod
     def generate_fake(count=100):
@@ -252,6 +254,21 @@ class Post(db.Model):
 db.event.listen(Post.body,'set',Post.on_changed_body)
 
 
+class Comment(db.Model):
+    __tablename__='comments'
+    id=db.Column(db.Integer,primary_key=True)
+    body=db.Column(db.Text)
+    body_html=db.Column(db.Text)
+    timestamp=db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    disabled=db.Column(db.Boolean)
+    author_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id=db.Column(db.Integer,db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags=['a','abbr','acronym','b','code','em','i','strong']
+        target.body_html=bleach.clean(markdown(value,output_format='html'),
+                tags=aallowed_tags,strip=True)
 
 
 
