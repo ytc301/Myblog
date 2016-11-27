0dfc14df2222861a1388cc2033170f4781e84e45
HymanLiu1987 <879651072@qq.com>
2016-11-27 14:20:56 +0800

    增加以令牌验证ID的方式

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -9,10 +9,15 @@ def vertify_password(email,password):
     if email=='':
         g.current_user=AnonymousUser()
         return True
+    if password=='':
+        g.current_user=User.vertify_auth_token(email_or_token)
+        g.token_used=True
+        return g.current_user is not None
     user=User.query.filter_by(email=email).first()
     if not user:
         return False
     g.current_user=user
+    g.token_used=False
     return user.verify_password(password)
 
 @auth.error_handler
@@ -30,3 +35,9 @@ def before_request():
     if not g.current_user is_anonymous and \
             not g.current_user.confirm:
         return forbidden('UNconfirmed account')
+
+@api.route('/token')
+def get_token():
+    if g.current_user.is_anonymous() or g.token_used:
+        return unauthorized('Invalid credentials')
+    return jsonify({'token':g.current_user.generate_auth_token(expiration=3600),'expiration':3600})
--- app/models.py
+++ app/models.py
@@ -211,6 +211,20 @@ class User(UserMixin,db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    def generate_auth_token(self,expiration):
+        s=Serializer(current_app.config['SECRET_KEY'],
+                expires_in=expiration)
+        return s.dumps({'id':self.id})
+
+    @staticmethod
+    def vertify_auth_token(token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
 class AnonymousUser(AnonymousUserMixin):
     def can(self,permissions):
         return False
